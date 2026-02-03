e803bf2f2e8e7f2c989bf388fa599353de3513a3
MAYANK <mmkumar0622@gmail.com>
2026-02-03 13:20:36 +0530

    Update from local upload to cloudinary for optimization

--- commentback.js
+++ commentback.js
@@ -1,25 +1,44 @@
 const multer = require("multer");
 const express = require("express");
+const cloudinary = require("cloudinary").v2;
+const {Cloudinarystorage} = require("multer-storage-cloudinary");
 const app = express();
 const cors = require("cors");
 const path = require("path");
+//const { resourceLimits } = require("worker_threads");
 const PORT = 3290;
+require("dotenv").config();
+
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+//app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+cloudinary.config({
+    cloud_name : process.env.cloud_folder,
+    api_key : process.env.cloud_key,
+    api_secret:process.env.cloud_secret
+})
 
 // diskstorage is used for where to upload,name,time stamp add of file
-const storeupload = multer.diskStorage({ 
-    destination:(req,file,cb)=>{
-        cb(null,"uploads/");
-    },
-    filename:(req,file,cb)=>{
-        const filename = Date.now() + "-" + file.originalname;
-        cb(null,filename);
+//const storeupload = multer.diskStorage({ 
+//    destination:(req,file,cb)=>{
+//        cb(null,"uploads/");
+//    },
+//    filename:(req,file,cb)=>{
+//        const filename = Date.now() + "-" + file.originalname;
+//        cb(null,filename);
+//    }
+//});  // aws S3,clousflare or cloundiary is better so i use it that here rather then diskstorage
+
+const storage = new Cloudinarystorage({
+    cloundiary:cloudinary,
+    params:{
+        folder:"media_uploads",
+        resource_type:"auto",
     }
-});
+})
 const uploads = multer({
-     storage : storeupload,
+     storage,
     limits : {
         fileSize: 150 * 1024 * 1024
     },
@@ -38,7 +57,8 @@ app.get("/",(req,res)=>{
 })
 app.post("/upload",uploads.single("media"),(req,res)=>{
    res.status(200).json({
-    url:`http://localhost:3290/uploads/${req.file.filename}`,
+    url:req.file.path,    
+    public_id:req.file.filename,                                         //`http://localhost:3290/uploads/${req.file.filename}`,
     msg:"File uploaded Successfully"
 });
 })
